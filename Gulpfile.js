a00266e56b4f1a4a578912e5ced8c61c169a7671
Derek Bytheway <derekbtw@gmail.com>
2018-10-30 11:03:51 -0600

    Live Pug reload

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,23 +3,47 @@ var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 var gulp_watch_pug = require('gulp-watch-pug');
+var connect = require('gulp-connect');
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+var config = {
+    bootStrapDir: './bower_components/bootstrap',
+    publicDir: './public'
+}
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'public',
+        livereload: true
+    });
+});
+
+//keeps gulp from crashing for scss errors & gives sass access to bootstrap
+gulp.task('sass', function() {
+    return gulp.src('./sass/*.scss')
+        .pipe(sass({ 
+            errLogToConsole: true,
+            includePaths: [config.bootStrapDir + '/assets/stylesheets']
+        }))
+        .pipe(gulp.dest(config.publicDir + '/css'));
 });
 
 gulp.task('views', function buildHTML() {
-    gulp.src('pug/**/*.pug')
-        .pipe(watch('pug/**/*.pug'))
-        .pipe(gulp_watch_pug('pug/**/*.pug', { delay: 100 }))
+    gulp.src('pug/*.pug')
+        .pipe(watch('pug/*.pug'))
+        .pipe(gulp_watch_pug('pug/*.pug', { delay: 100 }))
         .pipe(pug())
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./public'));
 });
 
-//Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
-    gulp.watch('pug/*.pug',['views']);
+gulp.task('livereload', function() {
+    gulp.src('./public/**/*')
+    .pipe(connect.reload());
 });
+
+gulp.task('watch', function() {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./pug/**/*.pug', ['views']);
+    gulp.watch(config.publicDir + '/**/*', ['livereload']);
+});
+
+gulp.task('default', ['connect', 'watch', 'sass']);
